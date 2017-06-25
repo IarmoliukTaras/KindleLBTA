848b7189f5a385a971b7411ba2061e87232c17d3
Iarmoliuk Taras <iarmoliuktaras@gmail.com>
2017-06-25 22:14:51 +0300

    Adding Subviews Using Frames

--- KindleLBTA/BookCell.swift
+++ KindleLBTA/BookCell.swift
@@ -10,10 +10,37 @@ import UIKit
 
 class BookCell: UITableViewCell {
     
+    let coverImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .orange
+        return imageView
+    }()
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "TARAS LOX"
+        return label
+    }()
+    
+    let authorLabel: UILabel = {
+        let label = UILabel()
+        label.text = " LOLOLO"
+        return label
+    }()
+    
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
         backgroundColor = .yellow
+        
+        addSubview(coverImageView)
+        coverImageView.frame = CGRect(x: 8, y: 8, width: 50, height: 64)
+        
+        addSubview(titleLabel)
+        titleLabel.frame = CGRect(x: 66, y: 20, width: 200, height: 20)
+        
+        addSubview(authorLabel)
+        authorLabel.frame = CGRect(x: 66, y: 48, width: 200, height: 20)
     }
     
     required init?(coder aDecoder: NSCoder) {
