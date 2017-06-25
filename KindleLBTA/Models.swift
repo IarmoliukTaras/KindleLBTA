50836036574020a77a2a018bd832955fcf6d5a32
Iarmoliuk Taras <iarmoliuktaras@gmail.com>
2017-06-25 19:12:19 +0300

    UITableView Cell Register and Dequeue

---
+++ KindleLBTA/Models.swift
@@ -0,0 +1,31 @@
+//
+//  Models.swift
+//  KindleLBTA
+//
+//  Created by 123 on 25.06.17.
+//  Copyright © 2017 taras team. All rights reserved.
+//
+
+import Foundation
+
+class Book {
+    let title: String
+    let author: String
+    let pages: [Page]
+    
+    init(title: String, author: String, pages: [Page]) {
+        self.author = author
+        self.title = title
+        self.pages = pages
+    }
+}
+
+class Page {
+    let number: Int
+    let text: String
+    
+    init(number: Int, text: String) {
+        self.number = number
+        self.text = text
+    }
+}
--- KindleLBTA/ViewController.swift
+++ KindleLBTA/ViewController.swift
@@ -8,16 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var books: [Book]?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+        navigationItem.title = "KindleLBTA"
+        tableView.tableFooterView = UIView()
+        setupBooks()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        
+        let book = self.books?[indexPath.row]
+        cell.textLabel?.text = book?.title
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let count = self.books?.count {
+            return count
+        }
+        return 0
+    }
+    
+    func setupBooks() {
+        let page = Page(number: 1, text: "text for the first page")
+        
+        let page1 = Page(number: 2, text: "text for second page")
+        
+        let book = Book(title: "Steve Jobs", author: "Walter Isaacson", pages: [page, page1])
+        
+        self.books = [book]
     }
 
 
